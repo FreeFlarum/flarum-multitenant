1ef87deed31a5ee9c11e388c6d4bd091d835dea1
Willem de Groot <gwillem@gmail.com>
2019-08-03 15:51:11 +0000

    Re-dynamic config.php using flarum.json

--- config.php
+++ config.php
@@ -1,24 +1,7 @@
-<?php return array (
-  'debug' => false,
-  'database' => 
-  array (
-    'driver' => 'mysql',
-    'host' => '127.0.0.1',
-    'port' => 3306,
-    'database' => 'test',
-    'username' => 'test',
-    'password' => 'kameel',
-    'charset' => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
-    'prefix' => '',
-    'strict' => false,
-    'engine' => 'InnoDB',
-    'prefix_indexes' => true,
-  ),
-  'url' => 'https://gwillem.freeflarum.com',
-  'paths' => 
-  array (
-    'api' => 'api',
-    'admin' => 'admin',
-  ),
-);
+<?php
+
+/* Generic, so I can dump the JSON data without confusing Flarum */
+$json = dirname(__FILE__) . "/../etc/flarum.json";
+$decoded = json_decode(file_get_contents($json), true);
+return $decoded;
+?>
