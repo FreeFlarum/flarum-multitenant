85c0dbb96f7cbfbd2d2b91891b9deda5fde842c6
Kevo <39010496+CWKevo@users.noreply.github.com>
2020-08-28 14:18:17 +0200

    Update footer layout
    • Added top and bottom margin to make it look nicer;
    • Spaces to make the code more readable;
    • Paragraph inside of the div;
    • display: inline-block !important to make hiding the footer harder;
    • Removed ID attribute because it was empty.
    
    Feel free to adjust anything.

--- vendor/flarum/core/views/frontend/app.blade.php
+++ vendor/flarum/core/views/frontend/app.blade.php
@@ -37,10 +37,11 @@
         </script>
 
         {!! $foot !!}
-@php
-if (!file_exists("/etc/hide_powered_by")) {
-        print('<div id="" style="text-align: center;">A <a href="https://www.freeflarum.com">free forum</a> powered by Free Flarum (<a href="https://www.freeflarum.com/docs/faq/#can-i-pay-to-remove-the-powered-by-freeflarum-footer">remove</a>)</div>');
-}
-@endphp
+
+        @php
+        if (!file_exists("/etc/hide_powered_by")) {
+            print('<div style="display: inline-block !important; text-align: center; margin: 5px 0;"><p>A <a href="https://www.freeflarum.com">free forum</a> powered by Free Flarum (<a href="https://www.freeflarum.com/docs/faq/#can-i-pay-to-remove-the-powered-by-freeflarum-footer">remove</a>)</p></div>');
+        }
+        @endphp
     </body>
 </html>
